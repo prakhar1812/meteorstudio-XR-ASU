e1e96544f72b5f780e2b20d7a6e4da2ed8bb1a98
Prakhar <pbajpaye@asu.edu>
2020-06-11 12:21:00 -0700

    6/11/2020

--- db/server.js
+++ db/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
-var port = 1234;
+var port = 3000;
 var MS = require("mongoskin");
 
 var db = MS.db("mongodb://127.0.0.1:27017/test")
