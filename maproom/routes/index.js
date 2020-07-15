e73b2848c53978f89271dcd6cae8598a21dda15d
gomarz <bra2091589@gmail.com>
2020-07-15 11:14:12 -0700

    revert erronoues push

--- maproom/routes/index.js
+++
@@ -1,7 +0,0 @@
-// var express = require('express');
-// 
-// const router = express.Router();
-// var loginController = require('./controller/loginController.js');
-// 
-// routes.post('/login', loginController.login(req, res));
-// routes.post('/createAccount', loginController.createAccount(req,res));
